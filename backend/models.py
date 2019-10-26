5e76c2ceb5293ffa49c2eec09580e322917d3899
BabouZ17 <dimitri.estrade@gmail.com>
2019-10-26 10:43:19 -0400

    dev, update of models

--- backend/models.py
+++ backend/models.py
@@ -2,79 +2,143 @@
 
 from datetime import Datetime
 from pytz import timezone
-from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-class Memory():
+Base = declarative_base()
+
+
+class Owner(Base):
+    __tablename__ = 'owner'
+    id = Column(Integer, primary_key=True)
+    first_name = Column('first_name', String(32))
+    last_name = Column('last_name', String(32))
+    email = Column('email', String(32), unique=True)
+    computers = relationship('Computer')
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
+
+
+class Memory(Base):
     __tablename__ = 'memory'
     id = Column(Integer, primary_key=True)
     model = Column('model', String(32), unique=True)
     frequency = Column('frequency', Integer, default=2133)
     maker = Column('maker', String(20))
-    created_on = Column('created_on', Datetime, default=Datetime.now(tz=timezone('America/Montreal')))
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
 
 
-class Cpu():
+class Cpu(Base):
     __tablename__ = 'cpu'
     id = Column(Integer, primary_key=True)
     model = Column('model', String(32), unique=True)
     frequency = Column('frequency', Float, default=2.0)
-    nb_cores = Colum('nb_cores', Integer, default=1)
+    nb_cores = Column('nb_cores', Integer, default=1)
     maker = Column('maker', String(20))
-    created_on = Column('created_on', Datetime, default=Datetime.now(tz=timezone('America/Montreal')))
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
 
 
-class Gpu():
+class Gpu(Base):
     __tablename__ = 'gpu'
     id = Column(Integer, primary_key=True)
     model = Column('model', String(32))
     ram = Column('ram', String(10), default=1)
     maker = Column('maker', String(20))
-    created_on = Column('created_on', Datetime, default=Datetime.now(tz=timezone('America/Montreal')))
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
 
 
-class Motherboard():
+class Motherboard(Base):
     __tablename__ = 'motherboard'
     id = Column(Integer, primary_key=True)
     model = Column('model', String(32))
     pcie_qty = Column('pcie_qty', Integer, default=1)
     sata_qty = Column('sata_qty', Integer, default=1)
     fans_qty = Column('fans_qty', Integer, default=1)
-    created_on = Column('created_on', Datetime, default=Datetime.now(tz=timezone('America/Montreal')))
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
 
 
-class HDD():
+class HDD(Base):
     __tablename__ = 'hdd'
     id = Column(Integer, primary_key=True)
     model = Column('model', String(32))
     maker = Column('maker', String(20))
     capacity = Column('capacity', Integer, default=1)
-    created_on = Column('created_on', Datetime, default=Datetime.now(tz=timezone('America/Montreal')))
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
 
 
-class SSD():
+class SSD(Base):
     __tablename__ = 'ssd'
     id = Column(Integer, primary_key=True)
     model = Column('model', String(32))
     maker = Column('model', String(20))
     capacity = Column('capacity', Integer, default=1)
-    created_on = Column('created_on', Datetime, default=Datetime.now(tz=timezone('America/Montreal')))
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
 
 
-class Computer():
+# Many to Many tables
+
+memory_association_table = Table('memory_association', Base.metadata,
+                                 Column('memory_id', Integer,
+                                        ForeignKey('memory.id')),
+                                 Column('computer_id', Integer,
+                                        ForeignKey('computer.id'))
+                                 )
+
+hdd_association_table = Table('hdd_association', Base.metadata,
+                              Column('hdd_id', Integer, ForeignKey('hdd.id')),
+                              Column('computer_id', Integer,
+                                     ForeignKey('computer.id'))
+                              )
+
+ssd_association_table = Table('ssd_association', Base.metadata,
+                              Column('ssd_id', Integer, ForeignKey('ssd.id')),
+                              Column('computer_id', Integer,
+                                     ForeignKey('computer.id'))
+                              )
+
+motherboard_association_table = Table('motherboard_association', Base.metadata,
+                                      Column('motherboard_id', Integer,
+                                             ForeignKey('motherboard.id')),
+                                      Column('computer_id', Integer,
+                                             ForeignKey('computer.id'))
+                                      )
+
+cpu_association_table = Table('cpu_association', Base.metadata,
+                              Column('cpu_id', Integer, ForeignKey('cpu.id')),
+                              Column('computer_id', Integer,
+                                     ForeignKey('computer.id'))
+                              )
+
+gpu_association_table = Table('gpu_association', Base.metadata,
+                              Column('gpu_id', Integer, ForeignKey('gpu.id')),
+                              Column('computer_id', Integer,
+                                     ForeignKey('computer.id'))
+                              )
+
+
+class Computer(Base):
     __tablename__ = 'computer'
     id = Column(Integer, primary_key=True)
     owner = Column('model', String(50))
     hdd_id = Column(Integer, ForeignKey('hdd.id'))
-    hdd = relationship('HDD', back_populates='computers')
+    hdd = relationship('hdds', back_populates='computers')
     ssd_id = Column(Integer, ForeignKey('sdd.id'))
-    ssd = relationship('HDD', back_populates='computers')
+    ssd = relationship('ssds', back_populates='computers')
     memory_id = Column(Integer, ForeignKey('memory.id'))
-    memory = relationship('HDD', back_populates='computers')
+    memory = relationship('memory', back_populates='computers')
     motherboard_id = Column(Integer, ForeignKey('motherboard.id'))
-    motherboard = 
+    motherboard = relationship('motherboard', back_populates='computers')
     cpu_id = Column(Integer, ForeignKey('cpu.id'))
-    cpu = 
+    cpu = relationship('cpus', back_populates='computers')
     gpu_id = Column(Integer, ForeignKey('gpu.id'))
-    gpu =  
-    created_on = Column('created_on', Datetime, default=Datetime.now(tz=timezone('America/Montreal')))
+    gpu = relationship('gpus', back_populates='computers')
+    owner_id = Column(Integer, ForeignKey('owner.id'))
+    created_on = Column('created_on', Datetime, default=Datetime.now(
+        tz=timezone('America/Montreal')))
