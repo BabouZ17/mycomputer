649b7e20f2cc745614210e9366db74a74307d846
BabouZ17 <dimitri.estrade@gmail.com>
2019-10-30 20:23:58 -0400

    dev, added json encoder

--- backend/encoder.py
+++ backend/encoder.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import DeclarativeMeta
 
 
 class CustomJSONEncoder(JSONEncoder):
+    """Custom encode for JSON"""
 
     def default(self, obj):
         if isinstance(obj.__class__, DeclarativeMeta):
